f774d67c9159ce585c768b7749ce719a88b0b078
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-08-08 21:21:13 -0700

    did suggested fixes

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ export default function Background() {
     return (
         <div style={{ display: "flex", flexDirection: "row" }}>
             <div>
-                <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6" }} className="text-black">
+                <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6", backgroundImage: "url(https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg)", backgroundSize: "contain"}}>
                     <div style={{ top: "auto", width: "578px", marginLeft: "100px", marginBottom: "50px", marginTop: "100px"}}>
                         <Card.Text style={{ fontSize: "2.5em", fontWeight: "bold" }}>
                             Order groceries for delivery <br /> or pickup today
